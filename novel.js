080d015f2395e8aecd44c6589328c5eddc260045
野間涼司 <hw15a153@oecu.jp>
2016-08-29 10:41:04 +0900

    master commit

---
+++ novel.js
@@ -0,0 +1,204 @@
+﻿/*
+ゲームの説明
+
+var novel[番号]={
+no:[番号],
+bg:[背景名],
+chara1:[キャラクター1名],
+chara2:[キャラクター2名],
+chara3:[キャラクター3名],
+effect:[エフェクト名],
+textaria:[テキストエリア名],
+text:[テキスト],
+choice1:[選択肢1テキスト],
+choice2:[選択肢2テキスト],
+choice3:[選択肢3テキスト],
+go1:[選択肢1行き先],
+go2:[選択肢2行き先],
+go3:[選択肢3行き先],
+bgm:[BGM名],
+se:[se名]
+clear:[ゲーム終了時の値。※今のところ存在だけを判断]
+};
+
+予約番号9999ロード用
+予約番号9998セーブ用
+
+基本的にデータは保持します。
+キャラクターを消したい時などは空のpngなどを用意してください。
+
+
+*/
+
+
+
+
+
+var novel1={
+no:1,
+text:"やさしい神さま",
+bg:"mizu"
+};
+var novel2={
+no:2,
+choice1:"初めから",
+choice2:"続きから",
+go1:3,
+go2:9999,
+};
+var novel3={
+no:3,
+bg:"kusa",
+textaria:"textbox",
+text:"神さまは月におわします。",
+chara2:"kami1"
+};
+var novel4={
+no:4,
+textaria:"textbox",
+text:"空を、海を、大地を、命をおつくりになりました。"
+
+};
+var novel5={
+no:5,
+text:"人と天使を愛する心やさしい神さま。",
+chara1:"hito1",
+chara3:"tenshi1"
+};
+var novel6={
+no:6,
+text:"楽園で人々と天使たちは神さまのお膝元、幸せに暮らしておりました。"
+};
+
+var novel7={
+ne:7,
+text:"ある日、人は神さまの言い付けを破ってしまいます。",
+chara1:"hito3"
+};
+var novel8={
+ne:8,
+text:"神さまはひどくお怒りになりました。",
+chara2:"kami3"
+};
+var novel9={
+ne:9,
+text:"心やさしい神さまは、人を楽園から追放することでその罪をお許しになられたのです。",
+chara1:"clear",
+chara2:"kami1"
+};
+var novel10={
+ne:10,
+text:"やさしい神さまはそれでも人を愛し、何もなかった地上に、光と水と風とをお与えになりま<br>した。",
+chara2:"kami2"
+};
+var novel11={
+ne:11,
+text:"楽園には、神さまにとても愛されていた天使がおりました。"
+};
+var novel12={
+ne:12,
+text:"天使は次第におごり、神さまになり代わって世界を自分のものにしようと、神さまに刃を向<br>けたのです。",
+chara3:"tenshi2"
+};
+var novel13={
+ne:13,
+text:"神さまは悲しみましたが、そのお力をもって天使を退けます。",
+chara2:"kami3",
+chara3:"clear"
+};
+var novel14={
+ne:14,
+bg:"machi",
+text:"やさしい神さまは天使を殺せず、羽根を焼き、地上へとおとしました。",
+chara2:"clear",
+chara3:"tenshi3"
+};
+var novel15={
+ne:15,
+text:"羽根を失った天使は神さまを憎み、神さまに愛される人を襲うようになりました。",
+chara1:"hito1"
+};
+var novel16={
+ne:16,
+text:"天使はやがて悪魔となり、醜い肉の翼で飛び人々を苦しめました。",
+chara1:"hito3",
+chara3:"akuma1"
+};
+var novel17={
+ne:17,
+text:"神さまは地上に遣いを出し、悪魔はまたしても退けられます。",
+chara1:"hito2",
+chara2:"tenshi2",
+chara3:"clear"
+};
+var novel18={
+no:18,
+choice1:"お礼を言う。",
+choice2:"何も言わない。",
+choice3:"逆ギレする。",
+go1:19,
+go2:30,
+go3:40
+};
+var novel19={
+no:19,
+text:"ありがとう。"
+};
+var novel20={
+ne:20,
+text:"そして神さまは、これからもずっと人を愛し、見守り続けて行くことをわたしたちにお告げに<br>なり、魔は鎮められ、人が滅ぶことは決してなくなったのです。",
+chara2:"kami2"
+};
+var novel21={
+ne:21,
+text:"神さまは月におわします。",
+chara1:"clear",
+chara2:"kami2"
+};
+var novel22={
+ne:22,
+text:"今もわたしたち人を愛し、守ってくださっているのです。"
+};
+var novel23={
+ne:23,
+clear:1
+};
+var novel30={
+no:30,
+text:"…。"
+};
+var novel31={
+no:31,
+text:"神は悲しくなりました。"
+};
+var novel32={
+no:32,
+text:"神は人を見放し、人々は絶滅しました。"
+};
+var novel33={
+ne:33,
+clear:1
+};
+var novel40={
+ne:40,
+text:"何で邪魔してんだよ<(｀^´)>"
+};
+var novel41={
+no:41,
+text:"神は悲しくなりました。"
+};
+var novel42={
+no:42,
+text:"神は人を見放し、人々は絶滅しました。"
+};
+var novel43={
+ne:43,
+clear:1
+};
+
+/*ここから下はシステムデータ*/
+var i=0;
+function dataset(){
+sessionStorage.noveldata=JSON.stringify(eval("novel"+i));
+}
+/*ここまでシステムデータ*/
---
+++ system.js
@@ -0,0 +1,310 @@
+﻿
+/*システムデータ*/
+var system_data={
+cleargame:0,
+flag1:"",
+flag2:""
+};
+
+/*表示しているデータ*/
+var now_data={
+no:"",
+bg:"",
+chara1:"",
+chara2:"",
+chara3:"",
+effect:"",
+textaria:"",
+text:"",
+choice1:"",
+choice2:"",
+choice3:"",
+go1:"",
+go2:"",
+go3:"",
+bgm:"",
+se:""
+};
+
+/*読み込み先データ*/
+var next_data;
+
+/*セーブロードフラグ*/
+var flag=0;
+
+/*読み込み場所データ*/
+var texter=0;
+
+/*各種ディレクトリルートのショートカット*/
+var bgroot="img/bg/";
+var chararoot="img/chara/";
+var effectroot="img/effect/";
+var textariaroot="img/box/";
+var bgmroot="bgm/";
+var seroot="se/";
+
+/*画面の初期化*/
+function dispinit(){
+document.getElementById("bg").src=now_data.bg="";
+document.getElementById("chara1").src=now_data.chara1=chararoot+"clear.png";
+document.getElementById("chara2").src=now_data.chara2=chararoot+"clear.png";
+document.getElementById("chara3").src=now_data.chara3=chararoot+"clear.png";
+document.getElementById("effect").src=now_data.effect="";
+document.getElementById("textaria").src=now_data.textaria="";
+document.getElementById("text").innerHTML=now_data.text="";
+document.getElementById("choice1").value=now_data.choice1="";
+document.getElementById("choice2").value=now_data.choice2="";
+document.getElementById("choice3").value=now_data.choice3="";
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+document.getElementById("bgm").src=now_data.bgm="";
+document.getElementById("se").src=now_data.se="";
+}
+
+/*動作メイン処理*/
+function mainexe(){
+
+/*次の場所を検索*/
+texter++;
+next_text();
+
+/*一時的に格納されたデータを読み込み*/
+next_data=JSON.parse(sessionStorage.noveldata);
+
+/*ナンバーとの同期*/
+now_data.no=texter;
+
+/*背景が変更になったときの処理*/
+if(next_data.bg){
+now_data.bg=bgroot+next_data.bg+".png";
+document.getElementById("bg").src=now_data.bg;
+}
+
+/*キャラクターが変更になったときの処理*/
+if(next_data.chara1){
+now_data.chara1=chararoot+next_data.chara1+".png";
+document.getElementById("chara1").src=now_data.chara1;
+}
+if(next_data.chara2){
+now_data.chara2=chararoot+next_data.chara2+".png";
+document.getElementById("chara2").src=now_data.chara2;
+}
+if(next_data.chara3){
+now_data.chara3=chararoot+next_data.chara3+".png";
+document.getElementById("chara3").src=now_data.chara3;
+}
+
+/*エフェクトが変更になったときの処理*/
+if(next_data.effect){
+now_data.effect=effectroot+next_data.effect+".png";
+document.getElementById("effect").src=now_data.effect;
+}
+
+/*テキストエリアが変更になったときの処理*/
+if(next_data.textaria){
+now_data.textaria=textariaroot+next_data.textaria+".png";
+document.getElementById("textaria").src=now_data.textaria;
+}
+
+/*テキストが変更になったときの処理*/
+if(next_data.text){
+now_data.text=next_data.text;
+document.getElementById("text").innerHTML=now_data.text;
+}
+
+/*ボタンが出現しているか判定*/
+if(next_data.choice1||next_data.choice2||next_data.choice3){
+
+/*nextボタンを消して進めなくする*/
+document.getElementById("next").style.display="none";
+
+/*ボタンの表示*/
+if(next_data.choice1){
+now_data.choice1=next_data.choice1;
+document.getElementById("choice1").value=now_data.choice1;
+document.getElementById("choice1").style.display="block";
+}
+if(next_data.choice2){
+now_data.choice2=next_data.choice2;
+document.getElementById("choice2").value=now_data.choice2;
+document.getElementById("choice2").style.display="block";
+}
+if(next_data.choice3){
+now_data.choice3=next_data.choice3;
+document.getElementById("choice3").value=now_data.choice3;
+document.getElementById("choice3").style.display="block";
+}
+
+}
+
+/*ボタンの非表示*/
+else{
+document.getElementById("next").style.display="block";
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+}
+
+/*bgmが変更になったときの処理*/
+if(next_data.bgm){
+if(next_data.bgm=="stop"){
+document.getElementById("bgm").stop();
+}
+else if(next_data.bgm=="play"){
+document.getElementById("bgm").play();
+}
+else{
+now_data.bgm=bgmroot+next_data+".mp3";
+document.getElementById("bgm").src=now_data.bgm;
+document.getElementById("bgm").play();
+}
+}
+
+/*seが変更になったときの処理*/
+if(next_data.se){
+if(next_data.se=="stop"){
+document.getElementById("se").stop();
+}
+else if(next_data.se=="play"){
+document.getElementById("se").play();
+}
+else{
+now_data.se=seroot+next_data+".mp3";
+document.getElementById("se").src=now_data.se;
+document.getElementById("se").play();
+}
+}
+
+/*ゲームをクリアした時の処理*/
+if(next_data.clear){
+system_data.cleargame++;
+texter=0;
+localStorage.nsd=JSON.stringify(system_data);
+dispinit();
+}
+i--;
+}
+
+/*menuを表示した時の処理*/
+function menuscreen(){
+flag=1;
+document.getElementById("next").style.display="none";
+document.getElementById("choice1").value="save";
+document.getElementById("choice1").style.display="block";
+document.getElementById("choice2").value="load";
+document.getElementById("choice2").style.display="block";
+document.getElementById("choice3").value="back";
+document.getElementById("choice3").style.display="block";
+}
+
+/*選択肢の処理*/
+function branch(f){
+if(eval("next_data.go"+f)==9998){
+saveset();
+}
+else if(eval("next_data.go"+f)==9999){
+loadset();
+}
+else{
+texter=eval("next_data.go"+f)-1;
+mainexe();
+}
+}
+
+/*セーブ処理*/
+function saveset(){
+flag=2;
+document.getElementById("choice1").value="save";
+document.getElementById("choice1").style.display="block";
+document.getElementById("choice2").value="delete";
+document.getElementById("choice2").style.display="block";
+document.getElementById("choice3").value="back";
+document.getElementById("choice3").style.display="block";
+}
+
+/*ロード処理*/
+function loadset(){
+flag=3;
+document.getElementById("choice1").value="load";
+document.getElementById("choice1").style.display="block";
+document.getElementById("choice2").value="delete";
+document.getElementById("choice2").style.display="block";
+document.getElementById("choice3").value="back";
+document.getElementById("choice3").style.display="block";
+}
+
+/*セーブ実行*/
+function saveexe(){
+localStorage.noveldata=JSON.stringify(now_data);
+flag=0;
+document.getElementById("next").style.display="block";
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+texter=now_data.no-1;
+mainexe();
+}
+
+/*削除実行*/
+function deleteexe(){
+flag=0;
+delete localStorage.noveldata;
+texter=now_data.no-1;
+document.getElementById("next").style.display="block";
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+mainexe();
+}
+
+/*バック実行*/
+function backexe(){
+flag=0;
+document.getElementById("next").style.display="block";
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+localStorage.nsd=JSON.stringify(system_data);
+texter=next_data.no-1;
+mainexe();
+}
+
+/*ロード実行*/
+function loadexe(){
+next_data=JSON.parse(localStorage.noveldata);
+flag=0;
+dispinit();
+document.getElementById("next").style.display="block";
+document.getElementById("bg").src=next_data.bg;
+document.getElementById("chara1").src=next_data.chara1;
+document.getElementById("chara2").src=next_data.chara2;
+document.getElementById("chara3").src=next_data.chara3;
+document.getElementById("effect").src=next_data.effect;
+document.getElementById("textaria").src=next_data.textaria;
+document.getElementById("text").innerHTML=next_data.text;
+document.getElementById("choice1").value=next_data.choice1;
+document.getElementById("choice2").value=next_data.choice2;
+document.getElementById("choice3").value=next_data.choice3;
+document.getElementById("choice1").style.display="none";
+document.getElementById("choice2").style.display="none";
+document.getElementById("choice3").style.display="none";
+document.getElementById("bgm").src=next_data.bgm;
+document.getElementById("se").src=next_data.se;
+
+texter=next_data.no-1;
+mainexe();
+}
+
+
+/*先システムデータ読み込み*/
+function flowchart(){
+dispinit();
+  if(!localStorage.nsd){
+   localStorage.nsd=JSON.stringify(system_data);
+  }
+  else{
+   system_data=JSON.parse(localStorage.nsd);
+  }
+mainexe();
+}
